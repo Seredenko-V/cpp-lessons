79df081f8f7b3e082dbf39dd864e555ccc239a91
Seredenko-V <seredenko.spb@gmail.com>
2022-08-05 16:26:13 +0300

    Класс MyVector. Конструкторы, деструктор, оператор =

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,35 +1,92 @@
 ﻿/******************************************************************************
 * 
-* Абрамян М. Э.
-* For 40. Даны целые числа A и B (A < B). Вывести все целые числа от A до B 
-* включительно; при этом число A должно выводиться 1 раз, число A + 1 должно 
-* выводиться 2 раза и т. д.
+* Класс MyVector с динамическим одномерным массивом
+* Конструктор по умолчанию, конструктор с параметром,
+* деструктор, оператор присваивания.
 * 
 *******************************************************************************/
 
 #include <iostream>
-#include <cmath>
 
 using namespace std;
+
+template <typename Type>
+class MyVector {
+public:
+	MyVector() noexcept = default;
+
+	// вызываем другой конструктор (для минимизации строк кода)
+	explicit MyVector(size_t size) 
+	: MyVector(size, Type()) {
+	}
+
+	MyVector(size_t size, const Type& value) 
+	: size_(size)
+	, capacity_(size) {
+		my_vector_ = new Type[size];
+		for (size_t i = 0; i < size; ++i) {
+			my_vector_[i] = value;
+		}
+	}
+
+	// Конструктор копирования
+	MyVector(const MyVector& other) 
+	: size_(other.size_) 
+	, capacity_(other.capacity_) {
+		my_vector_ = new Type[size_];
+		for (size_t i = 0; i < size_; ++i) {
+			this->my_vector_[i] = other.my_vector_[i];
+		}
+	}
+
+	~MyVector() {
+		delete[] my_vector_;
+		size_ = 0;
+	}
+
+	MyVector& operator=(const MyVector& rhs) {
+		if (this->my_vector_ == rhs.my_vector_) {
+			return *this;
+		}
+		this->size_ = rhs.size_;
+		// проверка на наличие данных в текущем векторе
+		if (this->my_vector_ != nullptr) {
+			delete[] my_vector_;
+		}
+		this->my_vector_ = new Type[size_];
+		for (size_t i = 0; i < size_; ++i) {
+			this->my_vector_[i] = rhs.my_vector_[i];
+		}
+		return *this;
+	}
+
+	void Print() {
+		for (size_t i = 0; i < size_; ++i) {
+			cout << my_vector_[i] << " "s;
+		}
+		cout << endl;
+	}
+
+private:
+	size_t size_ = 0;
+	size_t capacity_ = 0;
+	Type* my_vector_ = nullptr;
+};
 
 int main() {
 	setlocale(LC_ALL, "ru");
 
-	int A = 2;
-	int B = 5;
-	if (A >= B) {
-		cout << "A должно быть больше B"s << endl;
-		return 0;
-	}
+	MyVector<int> first(5, 2);
+	cout << "first = "s;
+	first.Print();
 
-	int count = 1;
-	for (int i = A; i <= B; ++i) {
-		for (int j = 0; j < count; ++j) {
-			cout << i << " ";
-		}
-		++count;
-		cout << endl;
-	}
+	MyVector<int> second(2, 9);
+	cout << "second = "s;
+	second.Print();
+
+	first = second;
+	cout << "first = "s;
+	first.Print();
 
 	return 0;
 }
