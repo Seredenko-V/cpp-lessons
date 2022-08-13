a5e4e6508e954ebc365736629cb95c51eae5f9f0
Seredenko-V <seredenko.spb@gmail.com>
2022-08-13 11:37:58 +0300

    Получение и изменение значения элемента std::set

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <set>
 #include <map>
+#include <iterator>
 
 using namespace std;
 
@@ -70,33 +71,54 @@ int main() {
 
 
 	/* SET */
-	//// хранит значения в единственном экземпляре, которые упорядоченны по возрастанию
-	//// (от меньшего к большему). При добавлении новых элементов сортировка сохраняется
-	//set<int> first_set = { 1,1,1,1,2,6,8,2 };
-	//for (size_t i = 0; i < first_set.size(); ++i) {
-	//	// РАЗОБРАТЬ ДОСТУП К ЭЛЕМЕНТУ
-	//}
-	//cout << "\n=== WORK INSERT ===\n";
-	//first_set.insert(0);
-	//for (const int& value : first_set) {
-	//	cout << value << " ";
-	//}
+	// хранит значения в единственном экземпляре, которые упорядоченны по возрастанию
+	// (от меньшего к большему). При добавлении новых элементов сортировка сохраняется
+	set<int> first_set = { 1,1,1,1,2,6,8,2 };
+	// доступ к элементам через итераторы
+	for (set<int>::iterator it = first_set.begin(); it != first_set.end(); ++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+	// или можно обратиться к конкретному элементу так:
+	size_t index_element = 2;
+	if (index_element >= first_set.size()) {
+		cout << "Недопустимое значение индекса"s << endl;
+	} else {
+		set<int>::iterator it_element = next(first_set.begin(), index_element);
+		cout << "Элемент set с индексом "s << index_element << " = "s << *it_element << endl;
+	}
+	// НО так никто почти не делает, т.к. у set двунаправленные итераторы, 
+	// а не произвольного доступа, см. https://habr.com/ru/post/122283/
+	
+	// если интересует какой-то конкретный элемент:
+	int value = 9;
+	set<int>::iterator it_value = find(first_set.begin(), first_set.end(), value);
+	if (it_value != first_set.end()) {
+		cout << "Через std::find = "s << *it_value << endl;
+	} else {
+		cout << "Элемент "s << value << " в first_set отсутствует"s << endl;
+	}
+	cout << "\n=== WORK INSERT ===\n"s;
+	first_set.insert(0);
+	for (const int& value : first_set) {
+		cout << value << " ";
+	}
 
 	/* MAP */
-	map<int, string> first_map = { {0, "first"}, {1, "first"}, {0, "second"} };
-	for (const auto& [key, value] : first_map) {
-		cout << "key = " << key << " value = " << value << endl;
-	}
-	//
-	cout << first_map.at(0) << endl;
-	first_map[0] = "blabla"s;
-	cout << first_map.at(0) << endl;
-	//
-	first_map[3] = "third"s;
-	for (const auto& [key, value] : first_map) {
-		cout << "key = " << key << " value = " << value << endl;
-	}
-	cout << "SIZE = " << first_map.size() << endl;
+	//map<int, string> first_map = { {0, "first"}, {1, "first"}, {0, "second"} };
+	//for (const auto& [key, value] : first_map) {
+	//	cout << "key = " << key << " value = " << value << endl;
+	//}
+	////
+	//cout << first_map.at(0) << endl;
+	//first_map[0] = "blabla"s;
+	//cout << first_map.at(0) << endl;
+	////
+	//first_map[3] = "third"s;
+	//for (const auto& [key, value] : first_map) {
+	//	cout << "key = " << key << " value = " << value << endl;
+	//}
+	//cout << "SIZE = " << first_map.size() << endl;
 
 	return 0;
 }
