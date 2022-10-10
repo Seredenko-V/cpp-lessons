ede316373ac1eb5c76e68e09db089eed653da8e8
Seredenko-V <seredenko.spb@gmail.com>
2022-10-10 11:12:01 +0300

    Масштаб изображения задается исходя из значений графика. Рисуется график, добавлен класс окружностей

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -4,6 +4,8 @@
 #include <utility> // для pair
 #include <fstream> // для работы с файлами например .txt
 #include <cassert> // для отладки
+#include <algorithm>
+#include <optional>
 
 using namespace std;
 
@@ -64,6 +66,14 @@ vector<int> GetCoordinatesX(const string& x_coord) {
     return result;
 }
 
+vector<int> GetCoordinatesY(const vector<pair<char, int>>& coord_y_and_text) {
+    vector<int> only_coord(coord_y_and_text.size());
+    for (size_t i = 0; i < only_coord.size(); ++i) {
+        only_coord[i] = coord_y_and_text[i].second;
+    }
+    return only_coord;
+}
+
 struct Point {
     Point() = default;
     Point(double x, double y)
@@ -72,6 +82,11 @@ struct Point {
     }
     double x = 0;
     double y = 0;
+};
+
+struct BordersImage {
+    uint32_t x = 100;
+    uint32_t y = 100;
 };
 
 ofstream& operator<<(ofstream& output, const Point& point) {
@@ -86,84 +101,151 @@ public:
     // Добавляет очередную вершину к ломаной линии
     Polyline& AddPoint(Point point) {
         points_.push_back(point);
+        return *this;
+    }
+
+    Polyline& SetColor(string&& color) {
+        color_ = move(color);
+        return *this;
+    }
+
+
+    ostream& GetTextSVG(ostream& out) const {
+        out << "<polyline points=\""sv;
+        bool is_first = true;
+        for (const Point& point : points_) {
+            if (is_first) {
+                out << point.x << ","sv << point.y;
+                is_first = false;
+            } else {
+                out << " "sv << point.x << ","sv << point.y;
+            }
+        }
+        out << "\" fill=\"none\" stroke=\"" << color_ << "\""sv;
+        out << "/>"sv;
+        return out;
     }
 
 private:
-    //void RenderObject(const RenderContext& context) const override;
-
-    std::vector<Point> points_;
+    vector<Point> points_;
+    string color_ = "black"s;
 };
 
-Polyline CreatePolilyne(const vector<int>& coord_x, const vector<pair<char, int>> coord_y) {
-    Polyline line;
-    // идем до coord_y.size() т.к. без значений для оси ординат график не построить
-    for (size_t i = 0; i < coord_y.size(); ++i) {
-
+/*
+* Класс Circle моделирует элемент <circle> для отображения круга
+* https://developer.mozilla.org/en-US/docs/Web/SVG/Element/circle
+*/
+class Circle {
+public:
+    Circle& SetCenter(Point center) {
+        center_ = center;
+        return *this;
+    }
+    Circle& SetRadius(double radius) {
+        radius_ = radius;
+        return *this;
+    }
+    Circle& SetColor(string&& color) {
+        color_ = move(color);
+        return *this;
     }
 
+    ostream& GetTextSVG(ostream& out) const {
+        out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
+        out << "r=\""sv << radius_ << "\" "sv;
+        out << "/>"sv;
+        return out;
+    }
+
+private:
+    Point center_ = { 0.0, 0.0 };
+    double radius_ = 1.0;
+    string color_ = "white"s;
+};
+
+// Перерасчет координат У для рисования графика в положительной плоскости (см. оси координат изображений)
+void SetOnlyPositiveValues(vector<int>& coordinates) {
+    // находим модуль минимального значения
+    uint32_t mod_min_value = abs(*min_element(coordinates.begin(), coordinates.end()));
+    // прибавляем ко всем элементам найденное значение (чтобы поднять график в положительную плоскость)
+    for (int& value : coordinates) {
+        value += mod_min_value + 1;
+    }
+}
+
+// инверсия координат для привычного восприятия графика (не перевернутого)
+void InversCoordinates(vector<int>& coordinates) {
+    for (int& value : coordinates) {
+        value *= -1;
+    }
+}
+
+void PrepareCoordinates(vector<int>& coordinates) {
+    InversCoordinates(coordinates);
+    SetOnlyPositiveValues(coordinates);
+}
+
+BordersImage FindBordersImage(const vector<int>& coord_x, const vector<int> coord_y) {
+    // +1 для того, чтобы отчетливо была видна минимальная точка
+    uint32_t max_x = *max_element(coord_x.begin(), coord_x.end()) + 1;
+    uint32_t max_y = *max_element(coord_y.begin(), coord_y.end()) + 1;
+    return { max_x, max_y };
+}
+
+
+vector<Point> CreateVectorPoint(const vector<int>& coord_x, const vector<int> coord_y) {
+    vector<Point> points(coord_y.size());
+    for (size_t i = 0; i < points.size(); ++i) {
+        points[i] = { static_cast<double>(coord_x[i]), static_cast<double>(coord_y[i]) };
+    }
+    return points;
+}
+
+Polyline CreatePolilyne(const vector<Point>& points) {
+    Polyline line;
+    // идем до coord_y.size() т.к. без значений для оси ординат график не построить
+    for (size_t i = 0; i < points.size(); ++i) {
+        line.AddPoint(points[i]);
+    }
     return line;
 }
 
-void Draw(vector<pair<string, string>>&& data, ostream& out) {
-    string begin_svg = "<svg viewBox=\"0 0 200 100\" xmlns=\"http://www.w3.org/2000/svg\">"s;
-    string line = "<polyline points=\"0, 100 50, 25 50, 75 100, 0\" />"s;
-    string end_svg = "</svg>"s;
-    out << begin_svg << line << end_svg;
+void Draw(BordersImage&& bourders, const vector<Point>& points, ostream& out) {
+    Polyline line = CreatePolilyne(points);
+
+    out << "<svg viewBox=\"0 0 "sv << bourders.x << " "sv << bourders.y << "\" xmlns=\"http://www.w3.org/2000/svg\">"sv;
+    line.GetTextSVG(out);
+    for (const Point& point : points) {
+        Circle circle;
+        circle.SetCenter(point).SetRadius(0.5);
+        circle.GetTextSVG(out);
+    }
+
+    out << "</svg>"sv;
 }
-
-//vector<Point> CreatePoints(const vector<int>& coord_x, const vector<int>& coord_y) {
-//    assert(coord_x.size() == coord_y.size());
-//    vector<Point> points(coord_x.size());
-//    for (size_t i = 0; i < coord_x.size(); ++i) {
-//        points[i] = { static_cast<double>(coord_x[i]), static_cast<double>(coord_y[i]) };
-//    }
-//    return points;
-//}
-
-vector<Point> InvCoordY() {
-
-    return {};
-}
-
-
 
 int main() {
     string name_file = "Task.txt"s;
     // считываем из файла матрицу (без текста задания)
     vector<string> data_file = ReadMatrixFromFile(name_file);
-    for (const string& str : data_file) {
-        cout << str << endl;
-    }
-    cout << "==========================" << endl;
 
     // получаем координаты оси абсцис из первой строки матрицы
     vector<int> Coordinates_X = GetCoordinatesX(data_file.front());
-    cout << "Coordinates_X = "s;
-    for (const int& value : Coordinates_X) {
-        cout << value << " "s;
-    }
-    cout << endl;
-    cout << "==========================" << endl;
 
     // получаем координаты оси ординат с имеющимися обозначениями (буквами)
     vector<pair<char, int>> data_matrix = ParseDataMatrix(move(data_file));
-    for (const auto& [liter, value] : data_matrix) {
-        cout << "liter = " << liter << '\t' << "value = " << value << endl;
-    }
-    cout << "==========================" << endl;
 
-    // получаем вектор точек, по которым будем строить график
+    vector<int> Coordinates_Y = GetCoordinatesY(data_matrix);
+    PrepareCoordinates(Coordinates_Y);
 
+    vector<Point> points = CreateVectorPoint(Coordinates_X, Coordinates_Y);
 
+    // добавить надписи к точкам
+    // сделать концы линии круглыми
 
-    //ofstream fout("image.svg");
+    ofstream fout("image.svg");
+    Draw(FindBordersImage(Coordinates_X, Coordinates_Y), points, fout);
 
-
-    ////Draw(move(result_parse), fout);
-    //vector<Point> points = CreatePoints(GetCoordinatesX(matrix.front()), GetCoordinatesY(result_parse));
-    ////for (const Point& point : points) {
-    ////    cout << point;
-    ////}
 
     return 0;
 }
