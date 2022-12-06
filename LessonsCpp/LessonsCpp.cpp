429469cdebb4708b7eaf2a24ed208a348293433a
Seredenko-V <seredenko.spb@gmail.com>
2022-12-06 21:05:53 +0300

    Лаб 4 работает Нужно заменить pow и Fact включить внутрь цикла, а не отдельной функцией.

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include  <cmath>
+#include <cassert>
 
 using namespace std;
 
@@ -10,18 +11,31 @@ int Fact(int n) {
 	return n * Fact(n - 1);
 }
 
-double CalculateFor(int n) {
+double CalculateFor(int n, double epsion) {
 	double result = 0;
-	for (int i = 1; i <= n; i += 2) {
-		result += 1 + (pow(-1, n) / Fact(2 * n + 1));
+	int number_element = 0;
+	double An_minus_one = 0;
+	double An = 0;
+
+	for (int i = 1; i <= n; ++i) {
+		An_minus_one = (1 + (pow(-1, i - 1) / Fact(2 * (i - 1) + 1)));
+		An = (1 + (pow(-1, i) / Fact(2 * i + 1)));
+		result = abs(An - An_minus_one);
+		//cout << "result = " << An << " - " << An_minus_one << " = " << result << endl;
+		if (result < epsion) {
+			number_element = i;
+			break;
+		}
 	}
-	return result;
+	cout << "An = " << An << endl;
+	cout << "pos = " << number_element << endl;
+	return An;
 }
 
 int main() {
-	int value = 0;
-	cin >> value;
-	cout << Fact(value) << endl;
+	//int value = 0;
+	//cin >> value;
+	//cout << Fact(value) << endl;
 
 	return 0;
 }
