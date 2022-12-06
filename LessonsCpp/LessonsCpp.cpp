552261a86c066d05943ffb15fb21fa3c497c2f69
Seredenko-V <seredenko.spb@gmail.com>
2022-12-06 21:19:20 +0300

    Попытка замены функции Fact

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -8,16 +8,26 @@ int Fact(int n) {
 	if (n == 0 || n == 1) {
 		return 1;
 	}
-	return n * Fact(n - 1);
+	int result = 1;
+	for (int i = 2; i <= n; ++i) {
+		result *= i;
+	}
+	return result;
 }
 
-double CalculateFor(int n, double epsion) {
+double CalculateFor(int n, double epsion, int& number_element) {
+	// заменить pow и Fact. Включить Fact внутрь этой функции
 	double result = 0;
-	int number_element = 0;
 	double An_minus_one = 0;
 	double An = 0;
 
+	int value_fact_minus_one = 1;
+	int value_fact = 2 * 1 + 1;
+
+	An = 1 + (pow(-1, 1) / value_fact);
+
 	for (int i = 1; i <= n; ++i) {
+
 		An_minus_one = (1 + (pow(-1, i - 1) / Fact(2 * (i - 1) + 1)));
 		An = (1 + (pow(-1, i) / Fact(2 * i + 1)));
 		result = abs(An - An_minus_one);
@@ -27,15 +37,17 @@ double CalculateFor(int n, double epsion) {
 			break;
 		}
 	}
-	cout << "An = " << An << endl;
-	cout << "pos = " << number_element << endl;
 	return An;
 }
 
 int main() {
-	//int value = 0;
-	//cin >> value;
-	//cout << Fact(value) << endl;
+	/*int n = 15;
+	double epsilon = 8.23131e-9;
+	int pos = 0;
+	double value_An = CalculateFor(n, epsilon, pos);
+	cout << "An = " << value_An << endl;
+	cout << "pos = " << pos << endl;*/
 
+	assert(Fact(7) == 5040);
 	return 0;
 }
