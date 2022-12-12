9513e579d5a303b78f6ebfaf88b035f5814632bf
Seredenko-V <seredenko.spb@gmail.com>
2022-12-12 16:03:31 +0300

    Функция сложения векторов

--- DynamicMemory/DynamicMemory.cpp
+++ DynamicMemory/DynamicMemory.cpp
@@ -1,98 +1,32 @@
 ﻿/*******************************************************************************
-    Работа с динамической памятью
-
-    Небольшая корректировка домашки. Дописал создание динамических двумерных
-    массивов для корректной передачи в функцию SumArr. 
-    Остальные ошибки постарайся найти сама, места где нужно посмотреть отметил
+    std::vector
 *******************************************************************************/
 
 #include <iostream> 
+#include <cassert>
+#include <vector>
+
 using namespace std;
 
-int** FillTArr(int str, int stolb)
-{
-    int** arr = new int* [str];
-    for (int i = 0; i < str; ++i)
-    {
-        arr[i] = new int[stolb];
+vector<int> SumVector(vector<int> a, vector<int> b) {
+    vector<int> result(a.size());
+    for (size_t i = 0; i < a.size(); ++i) {
+        result[i] = a[i] + b[i];
     }
-    return arr;
+    return result;
 }
 
-void Print(int** arr, int str, int stolb)
-{
-    for (int i = 0; i < str; ++i)
-    {
-        for (int j = 0; j < stolb; ++j)
-        {
-            cout << arr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-int** SumArr(int str, int stolb, int size_1, int size_2, int** arr_first, int** arr_second)
-{
-    int** arr = FillTArr(str, stolb);
-    for (int i = 0; i < size_1; ++i)
-    {
-        for (int j = 0; j < size_2; ++j)
-        {
-            arr[i][j] = arr_first[i][j] + arr_second[i][j];
-            //cout << arr[i][j] << ' ';
-        }
-        //cout << endl;
-    }
-    return arr;
-}
-
-void DelArr(int** arr, int str, int stolb)
-{
-    for (int i = 0; i < str; ++i) {
-        delete[] arr[i];
-    }
-    delete[] arr;
-}
-
-int main()
-{
-    // посмотри внимательнее на переменные ниже
-    int str = 5;
-    int stolb = 7;
-    const int size_1 = 2;
-    const int size_2 = 2;
-
-    int arr_first[size_1][size_2] = {
-        {1,2},
-        {2,9}
-    };
-    int arr_second[size_1][size_2] = {
-        {9,8},
-        {8,1}
-    };
-
-
-    // Это делается для того, чтобы передать двумерный массив в функцию
-    int** dinamic_arr_first = FillTArr(size_1, size_2);
-    for (int i = 0; i < size_1; ++i) {
-        for (int j = 0; j < size_1; ++j) {
-            dinamic_arr_first[i][j] = arr_first[i][j];
-        }
-    }
-    int** dinamic_arr_second = FillTArr(size_1, size_2);
-    for (int i = 0; i < size_1; ++i) {
-        for (int j = 0; j < size_1; ++j) {
-            dinamic_arr_second[i][j] = arr_second[i][j];
-        }
-    }
-
-    // и вот тут подумай какие из переменных тебе не нужно передавать
-    int** sum = SumArr(str, stolb, size_1, size_2, dinamic_arr_first, dinamic_arr_second);
-    Print(sum, str, stolb);
-    DelArr(sum, str, stolb);
+int main() {
+    setlocale(LC_ALL, "ru");
     
-    DelArr(dinamic_arr_first, str, stolb);
-    DelArr(dinamic_arr_second, str, stolb);
+    vector<int> first(5, 5);
+    vector<int> second(5, 3);
+    vector<int> result = SumVector(first, second);
+
+    for (size_t i = 0; i < result.size(); ++i) {
+        cout << result[i] << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
