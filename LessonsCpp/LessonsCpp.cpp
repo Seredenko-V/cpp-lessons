15b5c8cfb5e07a474f182ad69b33c685f33f2d95
Seredenko-V <seredenko.spb@gmail.com>
2023-01-21 13:22:54 +0300

    Сравнение скорости выполнения при копировании и без него

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,10 +1,34 @@
-﻿#include <iostream>
+﻿#include <chrono>
+#include <iostream>
 #include <cassert>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-vector<vector<int>> ParseValue(const vector<int>& elements) {
+class LogDuration {
+public:
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string name_block, std::ostream& output = std::cerr)
+        : name_block_(name_block) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << name_block_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    const std::string name_block_;
+};
+
+vector<vector<int>> ParseValueCopy(vector<int> elements) {
     vector<vector<int>> result;
     if (elements.empty()) {
         return result;
@@ -23,35 +47,38 @@ vector<vector<int>> ParseValue(const vector<int>& elements) {
     return result;
 }
 
-void Test()
-{
-    {
-        vector<int> elements = { 8,8,8,1,1,1,3 };
-        vector<vector<int>> etalon = { {8,8,8}, {1,1,1}, {3} };
-        assert(ParseValue(elements) == etalon);
+vector<vector<int>> ParseValueRef(const vector<int>& elements) {
+    vector<vector<int>> result;
+    if (elements.empty()) {
+        return result;
     }
-    {
-        vector<int> elements = { 8,1,3 };
-        vector<vector<int>> etalon = { {8}, {1}, {3} };
-        assert(ParseValue(elements) == etalon);
+    vector<int> box;
+
+    for (size_t i = 0; i < elements.size() - 1; ++i) {
+        box.push_back(elements[i]);
+        if (elements[i + 1] != elements[i]) {
+            result.push_back(box);
+            box.clear();
+        }
     }
-    {
-        vector<int> elements = { 8,1,1,1,3,6,3,6,8,8 };
-        vector<vector<int>> etalon = { {8}, {1,1,1}, {3}, {6}, {3}, {6}, {8,8} };
-        assert(ParseValue(elements) == etalon);
-    }
-    {
-        vector<int> elements = { 5 };
-        vector<vector<int>> etalon = { {5} };
-        assert(ParseValue(elements) == etalon);
-    }
-    {
-        vector<int> elements;
-        assert(ParseValue(elements).empty());
-    }   
+    box.push_back(elements.back());
+    result.push_back(box);
+    return result;
 }
 
 int main() {
-    Test();
+    const size_t SIZE = 9008000;
+    vector<int> elements(SIZE);
+    iota(elements.begin(), elements.end(), 1);
+
+    {
+        LogDuration dur("COPY");
+        ParseValueCopy(elements);
+    }
+    {
+        LogDuration dur("NO COPY");
+        ParseValueRef(elements);
+    }
+
     return 0;
 }
