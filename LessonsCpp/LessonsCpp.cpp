459ddd7d94fd5ed8854950b03fd44ac94a12b68c
Seredenko-V <seredenko.spb@gmail.com>
2023-01-23 17:59:38 +0300

    ЛР 3. Задание 1. Вариант 8

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,83 +1,61 @@
-﻿#include <chrono>
+﻿/***************************************************************
+    Лабораторная работа 3. Вариант 8
+    Изучение циклических алгоритмов, операторов цикла,
+    программирование циклического вычислительного процесса
+
+    Задание 1. Вычислить и вывести на экран или в файл в виде таблицы
+    значения функции, заданной графически, на интервале от Xнач до Xкон с
+    шагом dx. Интервал и шаг задать таким образом, чтобы проверить все ветви
+    программы. Таблица должна иметь заголовком и шапку.
+
+***************************************************************/
+
 #include <iostream>
-#include <cassert>
-#include <vector>
-#include <numeric>
+#include <cmath>
 
 using namespace std;
 
-class LogDuration {
-public:
-    using Clock = std::chrono::steady_clock;
-
-    LogDuration(const std::string name_block, std::ostream& output = std::cerr)
-        : name_block_(name_block) {
-    }
-
-    ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
-
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr << name_block_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-    }
-
-private:
-    const Clock::time_point start_time_ = Clock::now();
-    const std::string name_block_;
-};
-
-vector<vector<int>> ParseValueCopy(vector<int> elements) {
-    vector<vector<int>> result;
-    if (elements.empty()) {
-        return result;
-    }
-    vector<int> box;
-
-    for (size_t i = 0; i < elements.size() - 1; ++i) {
-        box.push_back(elements[i]);
-        if (elements[i + 1] != elements[i]) {
-            result.push_back(box);
-            box.clear();
-        }
-    }
-    box.push_back(elements.back());
-    result.push_back(box);
-    return result;
-}
-
-vector<vector<int>> ParseValueRef(const vector<int>& elements) {
-    vector<vector<int>> result;
-    if (elements.empty()) {
-        return result;
-    }
-    vector<int> box;
-
-    for (size_t i = 0; i < elements.size() - 1; ++i) {
-        box.push_back(elements[i]);
-        if (elements[i + 1] != elements[i]) {
-            result.push_back(box);
-            box.clear();
-        }
-    }
-    box.push_back(elements.back());
-    result.push_back(box);
-    return result;
-}
-
 int main() {
-    const size_t SIZE = 9008000;
-    vector<int> elements(SIZE);
-    iota(elements.begin(), elements.end(), 1);
+    int Xbegin = -10;
+    int Xend = 8;
+    double dx = 1; // шаг приращения
 
-    {
-        LogDuration dur("COPY");
-        ParseValueCopy(elements);
+    cout << "Table 1 - Function values"s << endl;
+    cout << "    x"s << "\t"s << "    y"s << endl;
+
+    // ========================= окружность =========================
+    int x_end_circle = -6;
+    // координаты центра окружности
+    int x_center_circle = -8;
+    int y_center_circle = -2;
+    int R = 2; // радиус окружности
+
+    for (double x = Xbegin; x < x_end_circle; x += dx) {
+        cout << x << "\t|"s << sqrt(R * R - pow(x - x_center_circle, 2)) + y_center_circle << endl;
     }
-    {
-        LogDuration dur("NO COPY");
-        ParseValueRef(elements);
+
+    // ========================= первая прямая =========================
+    int x_begin_line = x_end_circle;
+    int x_end_line = 2;
+    int y_begin_line = -2;
+    int y_end_line = 2;
+    for (double x = x_begin_line; x < x_end_line; x += dx) {
+        cout << x << "\t|"s << (x - x_begin_line) * (y_end_line - y_begin_line) / (x_end_line - x_begin_line) + y_begin_line << endl;
+    }
+
+    // ========================= вторая прямая =========================
+    int x_begin_zero_line = x_end_line;
+    int x_end_zero_line = 6;
+    int y_begin_zero_line = 0;
+    int y_end_zero_line = 0;
+    for (double x = x_begin_zero_line; x < x_end_zero_line; x += dx) {
+        cout << x << "\t|"s << (x - x_begin_zero_line) * (y_end_zero_line - y_begin_zero_line) / (x_end_zero_line - x_begin_zero_line) + y_begin_zero_line << endl;
+    }
+
+    // ========================= парабола =========================
+    int x_begin_parabola = x_end_zero_line;
+    for (double x = x_begin_parabola; x <= Xend; x += dx) {
+        cout << x << "\t|"s << pow(x - 6, 2) << endl;
     }
 
     return 0;
