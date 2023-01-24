714eac36c87e365610a67133798156c584e8dbd5
Seredenko-V <seredenko.spb@gmail.com>
2023-01-24 18:56:30 +0300

    Сортировка выбором

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,62 +1,56 @@
-﻿/***************************************************************
-    Лабораторная работа 3. Вариант 8
-    Изучение циклических алгоритмов, операторов цикла,
-    программирование циклического вычислительного процесса
-
-    Задание 1. Вычислить и вывести на экран или в файл в виде таблицы
-    значения функции, заданной графически, на интервале от Xнач до Xкон с
-    шагом dx. Интервал и шаг задать таким образом, чтобы проверить все ветви
-    программы. Таблица должна иметь заголовком и шапку.
-
-***************************************************************/
-
-#include <iostream>
-#include <cmath>
+﻿#include <iostream>
+#include <vector>
+#include <cassert>
 
 using namespace std;
 
+void Sort(vector<int>& elements) {
+    for (size_t i = 0; i < elements.size(); ++i) {
+        for (size_t j = i + 1; j < elements.size(); ++j) {
+            if (elements[j] < elements[i]) {
+                int tmp = elements[i];
+                elements[i] = elements[j];
+                elements[j] = tmp;
+            }
+        }
+    }
+}
+
+void Tests() {
+    {
+        vector<int> values = { -2,5,3,0 };
+        vector<int> etalon = { -2,0,3,5 };
+        Sort(values);
+        assert(values == etalon);
+    }
+    {
+        vector<int> values = { -2,-5,-3,0 };
+        vector<int> etalon = { -5,-3,-2,0 };
+        Sort(values);
+        assert(values == etalon);
+    }
+    {
+        vector<int> values = { 5,5,5,5,5,5 };
+        vector<int> etalon = { 5,5,5,5,5,5 };
+        Sort(values);
+        assert(values == etalon);
+    }
+    {
+        vector<int> values;
+        Sort(values);
+        assert(values.empty());
+    }
+    {
+        vector<int> values = { 5,0,0,0,0,3,3,3,3,1 };
+        vector<int> etalon = { 0,0,0,0,1,3,3,3,3,5 };
+        Sort(values);
+        assert(values == etalon);
+    }
+
+    cout << "All tests passed"s << endl;
+}
+
 int main() {
-    int Xbegin = -10;
-    int Xend = 8;
-    double dx = 1; // шаг приращения
-
-    cout << "Table 1 - Function values"s << endl;
-    cout << "    x"s << "\t"s << "    y"s << endl;
-
-    // ========================= окружность =========================
-    int x_end_circle = -6;
-    // координаты центра окружности
-    int x_center_circle = -8;
-    int y_center_circle = -2;
-    int R = 2; // радиус окружности
-
-    for (double x = Xbegin; x < x_end_circle; x += dx) {
-        cout << x << "\t|"s << sqrt(R * R - pow(x - x_center_circle, 2)) + y_center_circle << endl;
-    }
-
-    // ========================= первая прямая =========================
-    int x_begin_line = x_end_circle;
-    int x_end_line = 2;
-    int y_begin_line = -2;
-    int y_end_line = 2;
-    for (double x = x_begin_line; x < x_end_line; x += dx) {
-        cout << x << "\t|"s << (x - x_begin_line) * (y_end_line - y_begin_line) / (x_end_line - x_begin_line) + y_begin_line << endl;
-    }
-
-    // ========================= вторая прямая =========================
-    int x_begin_zero_line = x_end_line;
-    int x_end_zero_line = 6;
-    int y_begin_zero_line = 0;
-    int y_end_zero_line = 0;
-    for (double x = x_begin_zero_line; x < x_end_zero_line; x += dx) {
-        cout << x << "\t|"s << (x - x_begin_zero_line) * (y_end_zero_line - y_begin_zero_line) / (x_end_zero_line - x_begin_zero_line) + y_begin_zero_line << endl;
-    }
-
-    // ========================= парабола =========================
-    int x_begin_parabola = x_end_zero_line;
-    for (double x = x_begin_parabola; x <= Xend; x += dx) {
-        cout << x << "\t|"s << pow(x - 6, 2) << endl;
-    }
-
+    Tests();
     return 0;
 }
