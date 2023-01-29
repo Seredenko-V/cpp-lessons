9c2456c490db2edf60f1cc54288a171b5ca5a823
Seredenko-V <seredenko.spb@gmail.com>
2023-01-29 11:40:20 +0300

    Сравнение скорости выполнения при передаче по ссылки и по значению

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,34 +1,61 @@
-﻿#include <iostream>
+﻿#include <chrono>
+#include <iostream>
+#include <cassert>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
-int Sum(int& a, int& b) {
-    a += 7;
-    cout << "&a_foo = " << &a << endl;
-    cout << "&b_foo = " << &b << endl;
-    return a + b;
+class LogDuration {
+public:
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string name_block, std::ostream& output = std::cerr)
+        : name_block_(name_block) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << name_block_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    const std::string name_block_;
+};
+
+double AverageCopy(vector<double> elements) {
+    double result = 0.0;
+    for (const double& value : elements) {
+        result += value;
+    }
+    return result /= elements.size();
 }
 
-void Sort(vector<int>& elements) {
-    for (int& value : elements) {
-        value *= (-1);
+double AverageRef(const vector<double>& elements) {
+    double result = 0.0;
+    for (const double& value : elements) {
+        result += value;
     }
-}
-
-void Print(const vector<int>& elements) {
-    for (const int& value : elements) {
-        cout << value << ' ';
-    }
-    cout << endl;
+    return result /= elements.size();
 }
 
 int main() {
-    vector<int> test = { 2,6,-7,2,6,-2,0 };
-    cout << "before" << endl;
-    Print(test);
-    Sort(test);
-    cout << "after" << endl;
-    Print(test);
+    const size_t size = 10000000;
+    vector<double> test(size);
+    iota(test.begin(), test.end(), 1);
+    {
+        LogDuration timer("Copy");
+        cout << AverageCopy(test) << endl;
+    }
+    {
+        LogDuration timer("NO copy");
+        cout << AverageRef(test) << endl;
+    }
+
     return 0;
 }
