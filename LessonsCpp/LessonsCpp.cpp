0c4fd1a98c4945e1c9b7d71ded2d80032eadd034
Seredenko-V <seredenko.spb@gmail.com>
2023-01-29 11:23:20 +0300

    Примеры передачи параметров в функции по ссылке и по значению

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,34 +1,34 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
+int Sum(int& a, int& b) {
+    a += 7;
+    cout << "&a_foo = " << &a << endl;
+    cout << "&b_foo = " << &b << endl;
+    return a + b;
+}
+
+void Sort(vector<int>& elements) {
+    for (int& value : elements) {
+        value *= (-1);
+    }
+}
+
+void Print(const vector<int>& elements) {
+    for (const int& value : elements) {
+        cout << value << ' ';
+    }
+    cout << endl;
+}
+
 int main() {
-    int a = 4;
-    cout << "a = " << a << endl;
-    cout << "&a = " << &a << endl;
-
-    // ==================== указатель ===================
-    cout << "==================== указатель ===================" << endl;
-    int* ptr_a = &a;
-    cout << "ptr_a = " << ptr_a << endl;
-
-    int** ptr2_a = &ptr_a;
-    cout << "ptr2_a = " << ptr2_a << endl;
-    cout << "&ptr2_a = " << &ptr2_a << endl;
-
-    // разыменование
-    cout << "**ptr2_a = " << **ptr2_a << endl;
-    **ptr2_a = 9;
-
-    cout << "a = " << a << endl;
-
-    // ==================== ссылка ===================
-    cout << "==================== ссылка ===================" << endl;
-    int& ref_a = a;
-    cout << "ref_a = " << ref_a << endl;
-    cout << "&ref_a = " << &ref_a << endl;
-    ref_a = 2;
-    cout << "a = " << a << endl;
-
+    vector<int> test = { 2,6,-7,2,6,-2,0 };
+    cout << "before" << endl;
+    Print(test);
+    Sort(test);
+    cout << "after" << endl;
+    Print(test);
     return 0;
 }
