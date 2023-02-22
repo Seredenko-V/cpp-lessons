7867c00d63af6314ba4ed79f72a0e9c943398ba8
Seredenko-V <seredenko.spb@gmail.com>
2023-02-22 15:43:32 +0300

    Представление двумерного динамического массива в виде одномерного

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,32 +1,20 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <set>
-#include <string>
-#include <vector>
-#include <iterator>
+﻿#include <iostream>
 
 using namespace std;
 
-template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    for (auto it = range_begin; it != range_end; ++it) {
-        cout << *it << " "s;
-    }
-    cout << endl;
-}
-
-template <typename It>
-set<typename iterator_traits<It>::value_type> MakeSet(It range_begin, It range_end) {
-    return set<typename iterator_traits<It>::value_type>(range_begin, range_end);
-}
-
-//template <typename It>
-//auto MakeSet(It range_begin, It range_end) {
-//    return set(range_begin, range_end);
-//}
-
 int main() {
-    vector<string> langs = { "Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s, "C++"s, "C++"s, "Ruby"s, "Java"s };
-    set<string> unique_langs = MakeSet(langs.begin(), langs.end());
-    PrintRange(unique_langs.begin(), unique_langs.end());
+    int num_str = 2;
+    int num_col = 7;
+    int* matrix = new int[num_str * num_col];
+
+    for (int i = 0; i < num_str; ++i) {
+        for (int j = 0; j < num_col; ++j) {
+            matrix[i * num_str + j] = j;
+            cout << matrix[i * num_str + j] << ' ';
+        }
+        cout << endl;
+    }
+
+    delete[] matrix;
+    return 0;
 }
