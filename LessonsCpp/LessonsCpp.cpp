4a7500acfd1caacdd03cd08a5e1a79dd6a288ced
Seredenko-V <seredenko.spb@gmail.com>
2023-03-27 19:27:48 +0300

    Поиск ПОДпоследовательности, начинающейся с минимального элемента принимаемой последовательности. Длина подпоследовательностей не сравнивается. Находится подпоследовательность с первого минимального элемента,  т.е. { 9,2,35,2,6,7,2 } -> { 2,35 }

--- LessonsCpp/LessonsCpp.cpp
+++ LessonsCpp/LessonsCpp.cpp
@@ -1,53 +1,68 @@
 ﻿#include <iostream>
-#include <string>
 #include <vector>
-#include <stdexcept>
+#include <cassert>
 
 using namespace std;
 
-vector<int> operator+(const vector<int>& first, const vector<int>& second) {
-    if (first.size() != second.size()) {
-        throw logic_error("size1 != size2"s);
+
+// Поиск ПОДпоследовательности, начинающейся с минимального элемента принимаемой послед-ти
+// Длина подпослед-тей не сравнивается. Находится подпослед. с первого минимального элемента
+// т.е. { 9,2,35,2,6,7,2 } -> { 2,35 }
+vector<int> FindIncreaseSequenceWithMinimumStart(const vector<int>& seq) {
+    vector<int> increase;
+    increase.reserve(seq.size());
+    increase.push_back(seq.front());
+    bool is_new_seq = false;
+    for (int i = 1; i < seq.size(); ++i) {
+        if (seq[i] <= seq[i - 1]) {
+            is_new_seq = true;
+            if (increase.front() > seq[i]) {
+                increase.clear();
+                is_new_seq = false;
+            }
+        }
+        if (!is_new_seq) {
+            increase.push_back(seq[i]);
+        }
     }
-    vector<int> result(first.size());
-    for (size_t i = 0; i < result.size(); ++i) {
-        result[i] = first[i] + second[i];
-    }
-    return result;
+    return increase;
 }
 
-ostream& operator<<(ostream& out, const vector<int>& second) {
-    for (const int& value : second) {
-        cout << value << ' ';
-    }
-    return out;
+void Test() {
+    vector<int> sequence{ 3,10,60, 5,10,60 };
+    vector<int> result = FindIncreaseSequenceWithMinimumStart(sequence);
+    vector<int> etalon{ 3,10,60 };
+    assert(result == etalon);
+
+    sequence = { 3,10,60, 1,10,60 };
+    result = FindIncreaseSequenceWithMinimumStart(sequence);
+    etalon = { 1,10,60 };
+    assert(result == etalon);
+
+    sequence = { 3,10,60, 5,10,60, 1,10,60};
+    result = FindIncreaseSequenceWithMinimumStart(sequence);
+    etalon = { 1,10,60 };
+    assert(result == etalon);
+
+    sequence = { 1,1,1,1,1 };
+    result = FindIncreaseSequenceWithMinimumStart(sequence);
+    etalon = { 1 };
+    assert(result == etalon);
+
+    sequence = { 9,2,35,2,6,7,2 };
+    result = FindIncreaseSequenceWithMinimumStart(sequence);
+    etalon = { 2,35 };
+    assert(result == etalon);
+
+    //for (int v : result) {
+    //    cout << v << ' ';
+    //}
+    cout << "Tests is ok"s << endl;
 }
 
-vector<int>& operator++(vector<int>& first) {
-    for (size_t i = 0; i < first.size(); ++i) {
-        first[i]++;
-    }
-    return first;
-}
-
-vector<int> operator++(vector<int>& first, int fix) {
-    vector<int> tmp = first;
-    for (size_t i = 0; i < first.size(); ++i) {
-        first[i]++;
-    }
-    return tmp;
-}
 
 int main() {
-    vector<int> a{ 1,2,3,4,5 };
-    vector<int> b{ 9,8,7,6,5 };
-    try {
-        vector<int> c = a + b;
-        cout << c << endl;
-        vector<int> d = c++ + a;
-        cout << d << endl;
-    } catch (const logic_error& except) {
-        cout << except.what() << endl;
-    }
-    return 0;
+    Test();
+
+	return 0;
 }
