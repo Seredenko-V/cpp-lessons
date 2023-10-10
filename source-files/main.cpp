32d1d444952aa9dfb61fba6f43e2e6d5626f15d1
Seredenko-V <seredenko.spb@gmail.com>
2023-10-10 19:39:37 +0300

    Исправил

--- source-files/main.cpp
+++ source-files/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main() {
     cout << "Code in cpp-lessons"s << endl;
-    cout << "Branch is new-func"s << endl;
+    cout << "Branch is new-func. Fix bugs"s << endl;
     return 0;
 }
