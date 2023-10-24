ccf9988733fa7ada06edc3f1c757af9375d9d8ab
Seredenko-V <seredenko.spb@gmail.com>
2023-10-24 20:19:08 +0300

    Стек. Добавление и удаление последнего сообщения

--- source-files/main.cpp
+++ source-files/main.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
+#include <stack>
+#include <list>
 #include <string>
 
 using namespace std;
 
+template <typename T>
+class Stack {
+    // реализация))
+};
+
+bool IsAddMessage(const string& query) {
+    return query.front() == '+' ? true : false;
+}
+
+// Нужна т.к. в после числа есть '\n' и следующее считывание
+// наткнется на этот символ и получится пустая строка
+int ReadNumber(istream& in) {
+    string empty;
+    int number = 0;
+    in >> number;
+    getline(in, empty);
+    return number;
+}
+
 int main() {
-    cout << "Code in cpp-lessons"s << endl;
-    cout << "Created a new branch"s << endl;
-    cout << "Branch is new-func. Fix bugs"s << endl;
+    stack<uint32_t> messages;
+    list<uint32_t> deleted_messages;
+    cout << "Enter num operation "s;
+    int num_operation = ReadNumber(cin);
+    string query;
+
+    while(num_operation-- > 0) {
+        getline(cin, query);
+        if (IsAddMessage(query)) {
+            int value = stoi(query.substr(2));
+            messages.push(value);
+        } else {
+            if (!messages.empty()) {
+                deleted_messages.push_back(messages.top());
+                messages.pop();
+            }
+        }
+    }
+
+    cout << "\nDeleted messages:\n"s;
+    for (uint32_t message : deleted_messages) {
+        cout << message << endl;
+    }
+
     return 0;
 }
